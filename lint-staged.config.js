3d27da629b476ebe0f837bcf481026b6d29db98d
real-jacket <1762982273@qq.com>
2023-08-20 18:44:48 +0800

    fix(cya): fix root app lint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  '**/*.{js,jsx,ts,tsx}': ['eslint', 'prettier --write'],
+  '**/*.{js,jsx,ts,tsx}': ['eslint . --fix', 'prettier --write'],
   '**/*.json': ['prettier --write']
 };
