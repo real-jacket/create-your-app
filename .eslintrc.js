1a8244e88f9c76edf74c8e9316b6bfe604910be9
real-jacket <1762982273@qq.com>
2023-08-20 18:44:48 +0800

    fix(cya): fix root app lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,18 +6,18 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
+    // 'plugin:react/recommended',
+    // 'plugin:react/jsx-runtime',
     'plugin:@typescript-eslint/recommended',
     // 1. 接入 prettier 的规则
     'prettier',
     'plugin:prettier/recommended'
   ],
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  },
+  // settings: {
+  //   react: {
+  //     version: 'detect'
+  //   }
+  // },
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -27,12 +27,17 @@ module.exports = {
     sourceType: 'module'
   },
   // 2. 加入 prettier 的 eslint 插件
-  plugins: ['react', '@typescript-eslint', 'prettier'],
+  plugins: [
+    // 'react',
+    '@typescript-eslint',
+    'prettier'
+  ],
   rules: {
     // 3. 注意要加上这一句，开启 prettier 自动修复的功能
     'prettier/prettier': 'error',
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
-    'react/react-in-jsx-scope': 'off'
+    'no-require-imports': 'off'
+    // 'react/react-in-jsx-scope': 'off'
   }
 };
